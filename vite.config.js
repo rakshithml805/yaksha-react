33a02d834c60b49950d7af729520c490ca011e3c
Sonam Diwate <sonam.diwate@techademy.com>
2023-10-02 17:15:01 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,34 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+    proxy: {
+      '/api-identity': {
+        target: 'https://lx-platform.azurewebsites.net',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api-identity/, 'api'),
+      },
+      '/api-yaksha': {
+        target: 'https://yaksha-staging-core-api.azurewebsites.net',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api-yaksha/, 'api'),
+      },
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks (id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })

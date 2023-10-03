38de2dc450bca20c8f3dfc5a369fa56dce194019
Sonam Diwate <sonam.diwate@techademy.com>
2023-10-03 10:39:59 +0530

    helper added

---
+++ src/_shared/helper.js
@@ -0,0 +1,9 @@
+export const Role = {
+    superAdmin: "Superadmin",
+    tenantAdmin: "Tenantadmin",
+    tenantUser: "Tenantuser",
+    reviewer: "Reviewer",
+    evaluator: "Evaluator",
+    candidate: "Candidate",
+    author: "Author",
+};

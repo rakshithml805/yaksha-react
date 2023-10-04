913699aa2467ce7bda924a22733a7326dbceee0b
Sonam Diwate <sonam.diwate@techademy.com>
2023-10-04 10:53:11 +0530

    added routing

--- src/router.jsx
+++ src/router.jsx
@@ -129,7 +129,7 @@ const router = createBrowserRouter([
       element: (<Layout><AssessmentBankDetail /></Layout> ),
     },
     {
-      path: ":tenancyName/create-assessment-bank",
+      path: ":tenancyName/assessment-bank/create-assessment-bank",
       element: (<Layout><CreateAssessmentBank /></Layout> ),
     },
     {
